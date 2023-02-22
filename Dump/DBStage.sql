9b240acae8c47e4d6de30f98f147a85c58824855
FiXiK-Code <tailedstory@gmail.com>
2023-02-22 21:18:41 +0300

    add Dump

---
+++ Dump/DBStage.sql
@@ -0,0 +1,2 @@
+INSERT INTO mvp.DBStage (stageId,name,projectId) VALUES
+	 (0,'Инициализация',1);
---
+++ Dump/DBTaskStatus.sql
@@ -0,0 +1,5 @@
+INSERT INTO mvp.DBTaskStatus (name) VALUES
+	 ('На паузе'),
+	 ('Создана'),
+	 ('В работе'),
+	 ('Выполнена');
